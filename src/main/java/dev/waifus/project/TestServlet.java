301c532b9f6b73fdce865cccc2b70ececc79f1c3
Gabriel Costanza <costanzag1999@gmail.com>
2022-04-25 12:23:50 +0200

    First commit

---
+++ src/main/java/dev/waifus/project/TestServlet.java
@@ -0,0 +1,36 @@
+package dev.waifus.project;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String jsonResponse = "{\"message\": \"Success\"}";
+        this.outputResponse(resp, jsonResponse, 200);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //
+    }
+
+    private void outputResponse(HttpServletResponse response,String payload, int status){
+        response.setHeader("Content-Type", "application/json");
+        try{
+            response.setStatus(status);
+            if (payload != null){
+                OutputStream outputStream = response.getOutputStream();
+                outputStream.write(payload.getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
