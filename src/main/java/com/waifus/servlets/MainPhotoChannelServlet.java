f7f9c73b638639c7d44e256ac06463e2ef974e06
Gabriel Costanza <costanzag1999@gmail.com>
2022-06-03 20:24:43 +0200

    We did it!

--- src/main/java/com/waifus/servlets/MainPhotoChannelServlet.java
+++ src/main/java/com/waifus/servlets/MainPhotoChannelServlet.java
@@ -1,11 +1,30 @@
 package com.waifus.servlets;
 
+import com.waifus.services.PropertiesService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Properties;
 
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 10,      // 10 MB
+        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
 public class MainPhotoChannelServlet extends HttpServlet {
+
+    private Properties propHidden;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        // AQUI SE HARIA EL CAMBIO DE IDIOMA
+        this.propHidden = PropertiesService.getProperties("hidden");
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -13,6 +32,14 @@ public class MainPhotoChannelServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        Part filePart = request.getPart("photo");
+        Timestamp ts = Timestamp.from(Instant.now());
+        String fileName = ts.toString().replace("-","").replace(".","").replace(":","").replace(" ","") + ".png";
+        System.out.println(fileName);
+        System.out.println("HOLA");
+        for (Part part : request.getParts()) {
+            part.write(propHidden.getProperty("images.directory")+fileName);
+        }
     }
+
 }
