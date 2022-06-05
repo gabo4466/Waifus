aedc3c18f566b2c22dd2f3e97e357fc207f19794
Jaimercal <romerocal.jaime@gmail.com>
2022-06-05 17:45:07 +0200

    Images Servlet

--- src/main/java/com/waifus/servlets/ImagesServlet.java
+++ src/main/java/com/waifus/servlets/ImagesServlet.java
@@ -5,22 +5,14 @@ import com.waifus.services.PropertiesService;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.Properties;
 
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
-        maxFileSize = 1024 * 1024 * 10,      // 10 MB
-        maxRequestSize = 1024 * 1024 * 100   // 100 MB
-)
-public class MainPhotoChannelServlet extends HttpServlet {
 
+public class ImagesServlet extends HttpServlet {
     private Properties propHidden;
 
     @Override
@@ -29,19 +21,19 @@ public class MainPhotoChannelServlet extends HttpServlet {
         // AQUI SE HARIA EL CAMBIO DE IDIOMA
         this.propHidden = PropertiesService.getProperties("hidden");
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
+        String filename = URLDecoder.decode(request.getPathInfo().substring(1), "UTF-8");
+        File file = new File(propHidden.getProperty("images.directory"), filename);
+        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+        Files.copy(file.toPath(), response.getOutputStream());
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Timestamp ts = Timestamp.from(Instant.now());
-        String fileName = ts.toString().replace("-","").replace(".","").replace(":","").replace(" ","") + ".gif";
-        for (Part part : request.getParts()) {
-            part.write(propHidden.getProperty("images.directory")+fileName);
-        }
-    }
 
+    }
 }
