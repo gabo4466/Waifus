475f789e034d2aac30c0680e47bd1c7275976be1
Jaimercal <romerocal.jaime@gmail.com>
2022-06-08 17:40:50 +0200

    Servlet threads

--- src/main/java/com/waifus/servlets/MultimediaCreationServlet.java
+++ src/main/java/com/waifus/servlets/MultimediaCreationServlet.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.waifus.exceptions.MultimediaException;
 import com.waifus.model.Multimedia;
+import com.waifus.services.JWTService;
 import com.waifus.services.PropertiesService;
 import com.waifus.services.ResponseService;
 
@@ -13,15 +14,23 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Properties;
 
-@WebServlet(name = "MultimediaCreationServlet", value = "/MultimediaCreationServlet")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 10,      // 10 MB
+        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
 public class MultimediaCreationServlet extends HttpServlet {
+    private Properties propHidden;
     private Properties prop;
 
     @Override
     public void init() throws ServletException {
         super.init();
+        this.propHidden = PropertiesService.getProperties("hidden");
         // AQUI SE HARIA EL CAMBIO DE IDIOMA
         this.prop = PropertiesService.getProperties("config_es");
     }
@@ -34,24 +43,23 @@ public class MultimediaCreationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ResponseService<Multimedia> responseService = new ResponseService<Multimedia>();
-        Multimedia multimedia = new Gson().fromJson(req.getReader(), Multimedia.class);
+        String jwt = req.getHeader("Authorization");
+        int idThread = Integer.parseInt(req.getHeader("idThread"));
         try {
-            multimedia=multimedia.add();
-            JsonObject json = new JsonObject();
-            json.add("added", new JsonPrimitive("ok"));
-            json.add("multimedia", new Gson().toJsonTree(multimedia));
-            responseService.outputResponse(resp, json.toString(), 200);
-        }catch (MultimediaException e){
-            System.out.println(e.getMessage());
-            responseService.outputResponse(resp, responseService.errorResponse(e.getMessage()), 400);
-        }catch (SQLException e){
-            System.out.println(prop.getProperty("error.db"));
-            System.out.println(e.getMessage());
-            responseService.outputResponse(resp, responseService.errorResponse(prop.getProperty("error.db")), 400);
+            Multimedia multimedia = new Multimedia();
+            JWTService.verifyJWT(jwt);
+            Timestamp ts = Timestamp.from(Instant.now());
+            String fileName = ts.toString().replace("-","").replace(".","").replace(":","").replace(" ","") + ".png";
+            for (Part part : req.getParts()) {
+                part.write(propHidden.getProperty("images.directory")+fileName);
+            }
+            multimedia.setThread(idThread);
+            multimedia.setDirectory(fileName);
+            multimedia.add();
         }catch (Exception e){
             System.out.println(prop.getProperty("error.generic"));
             System.out.println(e.getMessage());
-            responseService.outputResponse(resp, responseService.errorResponse(e.getMessage()), 400);
+            responseService.outputResponse(resp, responseService.errorResponse(prop.getProperty("error.generic")), 400);
         }
     }
 }
